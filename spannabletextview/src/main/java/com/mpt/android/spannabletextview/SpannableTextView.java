52d9ff05c55b939afbffae5a2adf1f1bd8033e81
Sujith <kumarsujith683@gmail.com>
2021-08-18 06:06:50 -0700

    Modified Code comments

--- spannabletextview/src/main/java/com/mpt/android/spannabletextview/SpannableTextView.java
+++ spannabletextview/src/main/java/com/mpt/android/spannabletextview/SpannableTextView.java
@@ -12,12 +12,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * {@inheritDoc}
- * @author Milap Tank
+ * {@author Milap Tank}
  *         Email milaptank@gmail.com
  *         Project SpannableTextView - Android
  *         desc SpannableTextView.java  is for extra power to #TextView
- * @since 23/11/17  3:48 PM
+ * {@since 23/11/17  3:48 PM}
  */
 
 public class SpannableTextView extends Text {
